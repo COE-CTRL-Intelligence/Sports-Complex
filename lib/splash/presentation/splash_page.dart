4bee0be546311eb9665d313f2a7117ecb5d212dd
Wahabito <8257719+mailwahab247@gmail.com>
2022-07-26 21:47:40 +0000

    added bg_image to basic splash screen

--- lib/splash/presentation/splash_page.dart
+++ lib/splash/presentation/splash_page.dart
@@ -6,11 +6,22 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: const [Text("Hello World")],
+        body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/bg.jpg"),
+          fit: BoxFit.cover,
+        ),
       ),
-    );
+      child: const Center(
+        child: Text(
+          "Splash Screen",
+          style: TextStyle(
+            color: Colors.red,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    ));
   }
 }
