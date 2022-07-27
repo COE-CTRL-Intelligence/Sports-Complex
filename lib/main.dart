0b65831a1785c5656d445a16ca8aff7faa52ee6f
Wahabito <8257719+mailwahab247@gmail.com>
2022-07-27 16:00:26 +0000

    first prototype of splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sports_complex/core/presentation/app_widget.dart';
-import 'package:sports_complex/splash/presentation/splash_page.dart';
 
 void main() {
   runApp(AppWidget());
--- lib/splash/presentation/splash_page.dart
+++ lib/splash/presentation/splash_page.dart
@@ -5,23 +5,55 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/bg.jpg"),
-          fit: BoxFit.cover,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Splash Screen",
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 30,
-          ),
-        ),
-      ),
-    ));
+    return SafeArea(
+      child: Scaffold(
+          body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: const AssetImage("assets/images/bg.jpg"),
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.blue.withOpacity(0.25), BlendMode.darken)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 65,
+                      child: Image.asset(
+                        "assets/icons/dumbell.png",
+                        color: const Color(0xffFEF7C0),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text("SPORT",
+                            style: TextStyle(
+                              fontFamily: "DroidSans",
+                              color: Color(0xffFEF7C0),
+                              fontSize: 36,
+                            )),
+                        Text(
+                          "IFY",
+                          style: TextStyle(
+                            fontFamily: "DroidSans",
+                            color: Color(0xff83D475),
+                            fontSize: 36,
+                          ),
+                        ),
+                      ],
+                    )),
+                  ],
+                ),
+              ))),
+    );
   }
 }
