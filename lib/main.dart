eab87a77d7715c2ffe7be5eb35ef448060a716b0
Kwame_Opoku <kk.opoku@outlook.com>
2022-07-30 01:44:47 +0000

    updated code from master

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:sports_complex/src/myapp.dart';
-
-void main() => runApp(const MyApp());
-=======
 import 'package:sports_complex/widgets/app_widget.dart';
 
 void main() {
   runApp(AppWidget());
 }
->>>>>>> development
