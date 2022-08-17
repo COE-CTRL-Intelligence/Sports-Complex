4d7567d06ea90abebd8e75361e958256b502cd34
Kwame_Opoku <kk.opoku@outlook.com>
2022-08-17 13:27:39 +0000

    adde stuff

--- lib/pages/booking_page.dart
+++ lib/pages/booking_page.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class BookingPage extends StatelessWidget {
   const BookingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'hi, this the BOOKING PAGE',
-          style: TextStyle(fontSize: 20),
+    return Scaffold(
+      body: SafeArea(
+        child: SfCalendar(
+          view: CalendarView.week,
+          dataSource: MeetingDataSource(getAppointments()),
         ),
       ),
+      // bottomNavigationBar:
     );
+  }
+}
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime = DateTime(today.year, today.month, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+  meetings.add(Appointment(
+    startTime: startTime,
+    endTime: endTime,
+    subject: 'Conference',
+    color: Colors.green,
+  ));
+
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
   }
 }
