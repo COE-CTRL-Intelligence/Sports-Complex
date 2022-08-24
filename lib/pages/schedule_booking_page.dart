570954c02564845fdcd7497840435ccf58ad9c0f
Kwame_Opoku <kk.opoku@outlook.com>
2022-08-24 01:19:19 +0000

    worked on schedule booking page

--- lib/pages/schedule_booking_page.dart
+++ lib/pages/schedule_booking_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ffi';
+
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:sports_complex/widgets/custom_input_field.dart';
+import 'package:sports_complex/pages/routes/app_router.gr.dart';
 
 class ScheduleBookingPage extends StatelessWidget {
   const ScheduleBookingPage({Key? key}) : super(key: key);
@@ -8,41 +11,129 @@ class ScheduleBookingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final startDateController = TextEditingController();
     final endDateController = TextEditingController();
-    final startHourController = TextEditingController();
-    final endHourController = TextEditingController();
-    final startTimeCategory = TextEditingController();
-    final endTimeCategory = TextEditingController();
+    final startTimeController = TextEditingController();
+    final endTimeController = TextEditingController();
 
     return Scaffold(
       backgroundColor: const Color(0xffC2C3A0),
       appBar: AppBar(
+        title: const Text(
+          "NEW BOOKING",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
         backgroundColor: Colors.transparent,
-        elevation: 0.0,
+        elevation: 1.0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 10),
+            child: InkWell(
+              onTap: () {},
+              child: const Text(
+                "Add",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
       ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
+        padding: const EdgeInsets.only(bottom: 50),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              // Row(children: const [Text("Start")]),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //   children: [
-              //     CustomInputField(
-              //         fieldController: startDateController,
-              //         fieldName: "Start Date",
-              //         obscureText: false),
-              //     CustomInputField(
-              //         fieldController: startHourController,
-              //         fieldName: "Start Time",
-              //         obscureText: false),
-              //     CustomInputField(
-              //         fieldController: startTimeCategory,
-              //         fieldName: "Start Time",
-              //         obscureText: false)
-              //   ],
-              // )
+            children: [
+              const Text(
+                "STARTS",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    height: 40,
+                    width: 150,
+                    child: TextFormField(
+                      onTap: (null),
+                      cursorColor: Colors.black,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(13),
+                            borderSide: BorderSide.none),
+                      ),
+                      controller: startDateController,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: 105,
+                    child: TextFormField(
+                      onTap: (null),
+                      cursorColor: Colors.black,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(13),
+                            borderSide: BorderSide.none),
+                      ),
+                      controller: startTimeController,
+                    ),
+                  ),
+                ],
+              ),
+              const Text(
+                "ENDS",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    height: 40,
+                    width: 150,
+                    child: TextFormField(
+                      onTap: (null),
+                      cursorColor: Colors.black,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(13),
+                            borderSide: BorderSide.none),
+                      ),
+                      controller: endDateController,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                    width: 105,
+                    child: TextFormField(
+                      onTap: (null),
+                      cursorColor: Colors.black,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(13),
+                            borderSide: BorderSide.none),
+                      ),
+                      controller: endTimeController,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
