eebb5d80c000e5780d52700e681e8a0674069144
Wahabito <8257719+mailwahab247@gmail.com>
2022-08-26 23:47:04 +0000

    fixing homepage bugs ios

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:sports_complex/pages/routes/app_router.gr.dart';
 import 'package:sports_complex/widgets/background_image_widget.dart';
+import 'package:sports_complex/widgets/home_page_button.dart';
 import 'package:sports_complex/widgets/sportify_logo.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,40 +18,24 @@ class HomePage extends StatelessWidget {
         child: Scaffold(
           backgroundColor: Colors.transparent,
           body: Padding(
-            padding: const EdgeInsets.only(top: 100, bottom: 100),
+            padding: const EdgeInsets.symmetric(vertical: 80),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Hero(
                   tag: 'dumbell_logo',
-                  child: SizedBox(
+                  child: Image.asset(
                     height: 65,
-                    child: Image.asset(
-                      "assets/icons/dumbbell.png",
-                      color: const Color(0xffFEF7C0),
-                    ),
+                    "assets/icons/dumbbell.png",
+                    color: const Color(0xffFEF7C0),
                   ),
                 ),
                 const SizedBox(height: 45),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ElevatedButton(
-                      style: ButtonStyle(
-                        fixedSize:
-                            MaterialStateProperty.all(const Size(120, 120)),
-                        shape: MaterialStateProperty.all(
-                            const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20)))),
-                        backgroundColor: MaterialStateProperty.all(
-                          const Color.fromARGB(217, 217, 217, 217),
-                        ),
-                      ),
-                      onPressed: () {
-                        AutoRouter.of(context).push(const SelectSportRoute());
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+                    HomePageButton(
+                      icon: Column(
                         children: [
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -69,86 +54,38 @@ class HomePage extends StatelessWidget {
                           ),
                           Image.asset("assets/icons/soccerball.png",
                               color: Colors.black, height: 25),
-                          const Text('BOOK A FACILITY',
-                              style: TextStyle(
-                                fontSize: 11,
-                                color: Colors.black,
-                                height: 2.3,
-                                fontWeight: FontWeight.bold,
-                              ))
                         ],
                       ),
+                      title: 'BOOK FACILITY',
+                      onPressed: () {
+                        AutoRouter.of(context).push(const SelectSportRoute());
+                      },
                     ),
-                    ElevatedButton(
-                        style: ButtonStyle(
-                          fixedSize:
-                              MaterialStateProperty.all(const Size(120, 120)),
-                          shape: MaterialStateProperty.all(
-                              const RoundedRectangleBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(20)))),
-                          backgroundColor: MaterialStateProperty.all(
-                            const Color.fromARGB(217, 217, 217, 217),
-                          ),
-                        ),
-                        onPressed: () {
-                          AutoRouter.of(context)
-                              .push(const CheckBookingRoute());
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Image.asset(
-                              "assets/icons/calendar.png",
-                              height: 30,
-                            ),
-                            const Text(
-                              "CHECK BOOKING",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.bold,
-                                  height: 2),
-                            ),
-                          ],
-                        ))
+                    HomePageButton(
+                      icon: Image.asset(
+                        "assets/icons/calendar.png",
+                        height: 30,
+                      ),
+                      title: "CHECK BOOKING",
+                      onPressed: () {
+                        AutoRouter.of(context).push(const CheckBookingRoute());
+                      },
+                    ),
                   ],
                 ),
                 const SizedBox(
                   height: 15,
                 ),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(const Size(120, 120)),
-                    shape: MaterialStateProperty.all(
-                        const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20)))),
-                    backgroundColor: MaterialStateProperty.all(
-                      const Color.fromARGB(217, 217, 217, 217),
-                    ),
+                HomePageButton(
+                  icon: Image.asset(
+                    "assets/icons/dumbbell.png",
+                    color: Colors.black,
+                    height: 50,
                   ),
+                  title: "GYM",
                   onPressed: () {
                     AutoRouter.of(context).push(const GymLoginRoute());
                   },
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.values.last,
-                    children: [
-                      Image.asset(
-                        "assets/icons/dumbbell.png",
-                        color: Colors.black,
-                        height: 50,
-                      ),
-                      const Text(
-                        "GYM",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                        ),
-                      )
-                    ],
-                  ),
                 ),
                 const SizedBox(height: 45),
                 const Hero(
