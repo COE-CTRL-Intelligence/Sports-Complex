a82d6b84a07729e3faa6c4d72cba69e3b3b7588b
Claudia Yayra Joppa <yayrajoppa@Claudias-MacBook-Pro.local>
2022-08-27 03:23:15 +0000

    fixed bugs

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_const
+
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
--- lib/pages/routes/app_router.gr.dart
+++ lib/pages/routes/app_router.gr.dart
@@ -10,8 +10,8 @@
 //
 // ignore_for_file: type=lint
 
-import 'package:auto_route/auto_route.dart' as _i9;
-import 'package:flutter/material.dart' as _i10;
+import 'package:auto_route/auto_route.dart' as _i11;
+import 'package:flutter/material.dart' as _i12;
 
 import '../about_page.dart' as _i7;
 import '../booking_page.dart' as _i3;
@@ -20,66 +20,76 @@ import '../contact_us_page.dart' as _i8;
 import '../gym_login_page.dart' as _i5;
 import '../gym_signup_page.dart' as _i6;
 import '../home_page.dart' as _i2;
-import '../schedule_booking_page.dart' as _i6;
-import '../select_sport_page.dart' as _i7;
+import '../schedule_booking_page.dart' as _i9;
+import '../select_sport_page.dart' as _i10;
 import '../splash_page.dart' as _i1;
 
-class AppRouter extends _i9.RootStackRouter {
-  AppRouter([_i10.GlobalKey<_i10.NavigatorState>? navigatorKey])
+class AppRouter extends _i11.RootStackRouter {
+  AppRouter([_i12.GlobalKey<_i12.NavigatorState>? navigatorKey])
       : super(navigatorKey);
 
   @override
-  final Map<String, _i9.PageFactory> pagesMap = {
+  final Map<String, _i11.PageFactory> pagesMap = {
     SplashRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i1.SplashPage());
     },
     HomeRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i2.HomePage());
     },
     BookingRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i3.BookingPage());
     },
     CheckBookingRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i4.CheckBookingPage());
     },
     GymLoginRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i5.GymLoginPage());
     },
     GymSignUpRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i6.GymSignUpPage());
     },
     AboutRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i7.AboutPage());
     },
     ContactUsRoute.name: (routeData) {
-      return _i9.MaterialPageX<dynamic>(
+      return _i11.MaterialPageX<dynamic>(
           routeData: routeData, child: const _i8.ContactUsPage());
+    },
+    ScheduleBookingRoute.name: (routeData) {
+      return _i11.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i9.ScheduleBookingPage());
+    },
+    SelectSportRoute.name: (routeData) {
+      return _i11.MaterialPageX<dynamic>(
+          routeData: routeData, child: const _i10.SelectSportPage());
     }
   };
 
   @override
-  List<_i9.RouteConfig> get routes => [
-        _i9.RouteConfig(SplashRoute.name, path: '/'),
-        _i9.RouteConfig(HomeRoute.name, path: '/home'),
-        _i9.RouteConfig(BookingRoute.name, path: '/booking'),
-        _i9.RouteConfig(CheckBookingRoute.name, path: '/check_booking'),
-        _i9.RouteConfig(GymLoginRoute.name, path: '/gym_login'),
-        _i9.RouteConfig(GymSignUpRoute.name, path: '/gym_signup'),
-        _i9.RouteConfig(AboutRoute.name, path: '/About'),
-        _i9.RouteConfig(ContactUsRoute.name, path: '/Contact')
+  List<_i11.RouteConfig> get routes => [
+        _i11.RouteConfig(SplashRoute.name, path: '/'),
+        _i11.RouteConfig(HomeRoute.name, path: '/home'),
+        _i11.RouteConfig(BookingRoute.name, path: '/booking'),
+        _i11.RouteConfig(CheckBookingRoute.name, path: '/check_booking'),
+        _i11.RouteConfig(GymLoginRoute.name, path: '/gym_login'),
+        _i11.RouteConfig(GymSignUpRoute.name, path: '/gym_signup'),
+        _i11.RouteConfig(AboutRoute.name, path: '/About'),
+        _i11.RouteConfig(ContactUsRoute.name, path: '/Contact'),
+        _i11.RouteConfig(ScheduleBookingRoute.name, path: '/schedule_booking'),
+        _i11.RouteConfig(SelectSportRoute.name, path: '/select_sport_page')
       ];
 }
 
 /// generated route for
 /// [_i1.SplashPage]
-class SplashRoute extends _i9.PageRouteInfo<void> {
+class SplashRoute extends _i11.PageRouteInfo<void> {
   const SplashRoute() : super(SplashRoute.name, path: '/');
 
   static const String name = 'SplashRoute';
@@ -87,7 +97,7 @@ class SplashRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.HomePage]
-class HomeRoute extends _i9.PageRouteInfo<void> {
+class HomeRoute extends _i11.PageRouteInfo<void> {
   const HomeRoute() : super(HomeRoute.name, path: '/home');
 
   static const String name = 'HomeRoute';
@@ -95,7 +105,7 @@ class HomeRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i3.BookingPage]
-class BookingRoute extends _i9.PageRouteInfo<void> {
+class BookingRoute extends _i11.PageRouteInfo<void> {
   const BookingRoute() : super(BookingRoute.name, path: '/booking');
 
   static const String name = 'BookingRoute';
@@ -103,7 +113,7 @@ class BookingRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i4.CheckBookingPage]
-class CheckBookingRoute extends _i9.PageRouteInfo<void> {
+class CheckBookingRoute extends _i11.PageRouteInfo<void> {
   const CheckBookingRoute()
       : super(CheckBookingRoute.name, path: '/check_booking');
 
@@ -112,7 +122,7 @@ class CheckBookingRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i5.GymLoginPage]
-class GymLoginRoute extends _i9.PageRouteInfo<void> {
+class GymLoginRoute extends _i11.PageRouteInfo<void> {
   const GymLoginRoute() : super(GymLoginRoute.name, path: '/gym_login');
 
   static const String name = 'GymLoginRoute';
@@ -120,7 +130,7 @@ class GymLoginRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i6.GymSignUpPage]
-class GymSignUpRoute extends _i9.PageRouteInfo<void> {
+class GymSignUpRoute extends _i11.PageRouteInfo<void> {
   const GymSignUpRoute() : super(GymSignUpRoute.name, path: '/gym_signup');
 
   static const String name = 'GymSignUpRoute';
@@ -128,7 +138,7 @@ class GymSignUpRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i7.AboutPage]
-class AboutRoute extends _i9.PageRouteInfo<void> {
+class AboutRoute extends _i11.PageRouteInfo<void> {
   const AboutRoute() : super(AboutRoute.name, path: '/About');
 
   static const String name = 'AboutRoute';
@@ -136,8 +146,26 @@ class AboutRoute extends _i9.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i8.ContactUsPage]
-class ContactUsRoute extends _i9.PageRouteInfo<void> {
+class ContactUsRoute extends _i11.PageRouteInfo<void> {
   const ContactUsRoute() : super(ContactUsRoute.name, path: '/Contact');
 
   static const String name = 'ContactUsRoute';
+}
+
+/// generated route for
+/// [_i9.ScheduleBookingPage]
+class ScheduleBookingRoute extends _i11.PageRouteInfo<void> {
+  const ScheduleBookingRoute()
+      : super(ScheduleBookingRoute.name, path: '/schedule_booking');
+
+  static const String name = 'ScheduleBookingRoute';
+}
+
+/// generated route for
+/// [_i10.SelectSportPage]
+class SelectSportRoute extends _i11.PageRouteInfo<void> {
+  const SelectSportRoute()
+      : super(SelectSportRoute.name, path: '/select_sport_page');
+
+  static const String name = 'SelectSportRoute';
 }
