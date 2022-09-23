6a1efa8d893666784e108343e4a69716fdeb8198
Adoma <adomahobeng@gmail.com>
2022-09-23 18:22:02 +0000

    git add git commit -m <Update> -m <Add changes for about page>

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,14 +1,72 @@
 import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
-  const AboutPage({Key? key}) : super(key: key);
+  AboutPage({Key? key}) : super(key: key);
+
+late double height,width;
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hi, this is the about page'),
+  Widget build(BuildContext context) {   
+height=MediaQuery.of(context).size.height;
+    width = MediaQuery.of(context).size.width;
+    return Scaffold( 
+      
+      backgroundColor: Color(0xC7FFFFFF),
+      appBar: AppBar( elevation: 0,backgroundColor: Colors.transparent,
       ),
-    );
+        body:Padding(padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 500),
+         child: Column(children: const [
+          SizedBox(
+           height: 15,
+           width: 80,
+           child: Text('About',
+           style:TextStyle(fontSize: 16,
+            fontWeight:FontWeight.bold , 
+          ),),
+          ),
+         
+          SizedBox(
+           height: 40,
+           width: 500,
+           child: Text(' \n______________________________________________________________',
+           style:TextStyle(fontSize: 16,wordSpacing: 0,
+          ),),
+          ),
+
+          SizedBox(
+           child: Text('Description',
+           style:TextStyle(fontSize: 16,
+            fontWeight:FontWeight.bold ,   
+          ),),
+          ),
+          
+          SizedBox(
+           height: 230,
+           width:400,
+           child: Text('              Sportify is a mobile app for \n              students of Kwame Nkrumah University of \n              Science and Technology and non-stuudent \n              which serves as a mobile \n              companion for the GUSSS sports complex.\n              Its main objective is to bring \n              functions related to using \n              the GUSSS sports complex to mobile \n              devices.',
+           style:TextStyle(fontSize: 16,
+           ),),
+          ),
+
+          SizedBox(
+           height: 40,
+           width: 100,
+           child: Text('\n   Developer',
+           style:TextStyle(fontSize: 16,
+            fontWeight:FontWeight.bold ,  
+          ),),
+          ),
+
+           SizedBox(
+           height: 100,
+           width: 200,
+           child: Text('        Kwame Nkrumah \n     University of Science \n      and Technology \n             (KNUST)',
+           style:TextStyle(fontSize: 16,  
+          ),),
+          ),
+          ],
+          )
+        ),
+      );
   }
 }
